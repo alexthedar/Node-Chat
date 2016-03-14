237627fe9b9ed9999afc8c2a8ce34323aed11d25
alexthedar <alex.castaneda@gmail.com>
2016-03-14 11:17:36 -0700

    add styling.

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
+'use strict'
+
 var express = require('express');
 var app = express();
+
+app.use(express.static('public'));
+
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client){
+  console.log('client connected');
+
+  client.emit('message', {hello: 'world'});
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+server.listen(8080);
