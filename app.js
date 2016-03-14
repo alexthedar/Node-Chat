5ad2ecf74cdc5d93afec470040d542a1993382e8
alexthedar <alex.castaneda@gmail.com>
2016-03-14 15:28:10 -0700

    sending from client recieving on server sending from server recieiving from server.

--- app.js
+++ app.js
@@ -5,21 +5,20 @@ var app = express();
 
 app.use(express.static('public'));
 
-
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function(client){
-  console.log('client connected');
-  client.on('messages', function(data){
-    client.broadcast.emit('message', data);
-    console.log(data);
-  });
-});
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-
 });
 
 server.listen(8080);
+
+io.on('connection', function(client){
+  console.log('client connected');
+
+  client.on('message', function(data){
+    client.broadcast.emit('message', data);
+    client.emit('message',data);
+  });
+});
