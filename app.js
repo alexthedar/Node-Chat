a68c59b132ec92a9b2f072fc36c0616ac23171f1
alexthedar <alex.castaneda@gmail.com>
2016-03-15 09:35:59 -0700

    add redis. save only 10 messages to redis

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var app = express();
+var redis = require('redis');
+var redisClient = redis.createClient();
 
 app.use(express.static('public'));
 
@@ -14,16 +16,40 @@ app.get('/', function(req, res){
 
 server.listen(8080);
 
+var storeMessage = function(name, data){
+  var message = JSON.stringify({name:name,data:data});
+
+  redisClient.lpush("messages", message, function(err, res){
+    redisClient.ltrim("messages",0,9);
+  });
+};
+
 io.on('connection', function(client){
   console.log('client connected');
 
   client.on('join', function(name){
     client.username=name;
-  })
+
+
+    redisClient.lrange("messages",0,-1,function(err,messages){
+      messages.reverse();
+
+      messages.forEach(function(message){
+        message = JSON.parse(message);
+        client.emit("message", message.name + ": " + message.data);
+      });
+      
+      console.log(client.username + " has joined");
+      client.broadcast.emit('message', client.username + " has joined.");
+      client.emit('message', client.username + " has joined.");
+    });
+
+  });
 
   client.on('message', function(data){
     var username = client.username;
     client.broadcast.emit('message', username + ": " + data);
     client.emit('message', username + ": " + data);
+    storeMessage(username, data);
   });
 });
